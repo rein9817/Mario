33f9948450010ac9334a909b9ef9b155b521c4bd
rein9817 <erickklu1014@gmail.com>
2024-05-18 15:02:49 +0800

    test

--- assets/script/Menu.ts
+++ assets/script/Menu.ts
@@ -12,16 +12,12 @@ export default class NewClass extends cc.Component {
         log_in_btn.target = this.node;
         log_in_btn.component = "Menu";
         log_in_btn.handler = "loadLoginScene";
-        cc.find("Canvas/LoginButton")
-            .getComponent(cc.Button)
-            .clickEvents.push(log_in_btn);
+        cc.find("Canvas/LoginButton").getComponent(cc.Button).clickEvents.push(log_in_btn);
         let sign_up_btn = new cc.Component.EventHandler();
         sign_up_btn.target = this.node;
         sign_up_btn.component = "Menu";
         sign_up_btn.handler = "loadSignupScene";
-        cc.find("Canvas/RegisterButton")
-            .getComponent(cc.Button)
-            .clickEvents.push(sign_up_btn);
+        cc.find("Canvas/RegisterButton").getComponent(cc.Button).clickEvents.push(sign_up_btn);
         cc.audioEngine.playMusic(this.bgm, true);
     }
 
