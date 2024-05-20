e761bfbb47408bfff8592a4d99f33cce4229c3ae
rein9817 <erickklu1014@gmail.com>
2024-05-21 03:24:12 +0800

    fixing camera

--- assets/script/camera.ts
+++ assets/script/camera.ts
@@ -1,4 +1,4 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class CameraFollow extends cc.Component {
@@ -9,27 +9,26 @@ export default class CameraFollow extends cc.Component {
     smoothTime: number = 0.2; // 控制相機跟隨的平滑時間
 
     @property
-    minX: number = 0; // 相機X軸最小位置
+    minX: number = 0; 
     @property
-    maxX: number = 3000; // 相機X軸最大位置
+    maxX: number = 3000; 
     @property
-    minY: number = 0; // 相機Y軸最小位置
+    minY: number = 0; 
     @property
-    maxY: number = 500; // 相機Y軸最大位置
+    maxY: number = 0 
 
     update(dt) {
         if (this.player) {
             let targetPos = this.player.position;
             let cameraPos = this.node.position;
-    
+
             let newX = cc.misc.lerp(cameraPos.x, targetPos.x, this.smoothTime);
             let newY = cc.misc.lerp(cameraPos.y, targetPos.y, this.smoothTime);
-    
+
             newX = cc.misc.clampf(newX, this.minX, this.maxX);
             newY = cc.misc.clampf(newY, this.minY, this.maxY);
-    
+
             this.node.position = new cc.Vec2(newX, newY);
         }
     }
-    
 }
