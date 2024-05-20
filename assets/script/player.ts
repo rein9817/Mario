54bc499eb7fdd032d51f7ac2aef080e7be5a20b4
rein9817 <erickklu1014@gmail.com>
2024-05-21 03:13:11 +0800

    fixing level select

--- assets/script/player.ts
+++ assets/script/player.ts
@@ -1,27 +1,30 @@
 import Global from "./global";
-
-
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Player extends cc.Component {
+    @property(cc.Node)
+    game_mgr: cc.Node = null;
+
+    @property()
+    is_inwater: boolean = false;
 
     @property()
     jumpspeed: number = 300;
 
-    @property(cc.AudioClip)
-    game_over_sound: cc.AudioClip = null;
-    
     private _animation: cc.Animation = null;
     private _animState: cc.AnimationState = null;
     private _idleAnimState: cc.AnimationState = null;
     private _moveAnimState: cc.AnimationState = null;
     private _jumpAnimState: cc.AnimationState = null;
     private _dieAnimState: cc.AnimationState = null;
+    private _swimAnimState: cc.AnimationState = null;
+    private idleFrame: cc.SpriteFrame = null;
+
     private is_onGround: boolean = false;
     private is_die: boolean = false;
 
-    private moveDir = 0;
+    private moveDir = cc.v2(0, 0);
 
     @property()
     playerSpeed: number = 100;
@@ -33,24 +36,41 @@ export default class Player extends cc.Component {
     }
 
     start() {
+        this.idleFrame = this.getComponent(cc.Sprite).spriteFrame;
+
         this._idleAnimState = this._animation.getAnimationState("player_idle");
         this._moveAnimState = this._animation.getAnimationState("player_move");
         this._jumpAnimState = this._animation.getAnimationState("player_jump");
         this._dieAnimState = this._animation.getAnimationState("player_die");
-        this._animState = this._animation.play("player_idle");
+        this._swimAnimState = this._animation.getAnimationState("player_swim");
+
+        this.updateAnimationState();
     }
 
     update(dt) {
-        this.getComponent(cc.RigidBody).linearVelocity = cc.v2(
-            this.moveDir * this.playerSpeed,
-            this.getComponent(cc.RigidBody).linearVelocity.y
-        );
+        let velocity = this.moveDir.mul(this.playerSpeed);
+        if (this.is_inwater) {
+            velocity = cc.v2(this.moveDir.x * this.playerSpeed, this.moveDir.y * this.playerSpeed);
+        }
+        this.getComponent(cc.RigidBody).linearVelocity = velocity;
 
-        if (this.moveDir !== 0) {
-            this.node.setScale(new cc.Vec2(this.moveDir, 1));
+        if (this.moveDir.x !== 0) {
+            this.node.setScale(new cc.Vec2(Math.sign(this.moveDir.x), 1));
         }
 
-        if (!this.is_die) {
+        this.updateAnimationState();
+    }
+
+    updateAnimationState() {
+        if (this.is_die) {
+            if (this._animState !== this._dieAnimState) {
+                this._animState = this._animation.play("player_die");
+            }
+        } else if (this.is_inwater) {
+            if (this._animState !== this._swimAnimState) {
+                this._animState = this._animation.play("player_swim");
+            }
+        } else {
             const velocity = this.getComponent(cc.RigidBody).linearVelocity;
             if (!velocity.fuzzyEquals(cc.Vec2.ZERO, 0.01) && this.is_onGround) {
                 if (this._animState !== this._moveAnimState) {
@@ -65,30 +85,25 @@ export default class Player extends cc.Component {
                     this._animState = this._animation.play("player_idle");
                 }
             }
-        } else {
-            if (this._animState !== this._dieAnimState) {
-                this._animState = this._animation.play("player_die");
-            }
         }
     }
 
-    playerMove(moveDir: number) {
+    playerMove(moveDir: cc.Vec2) {
         this.moveDir = moveDir;
     }
 
     playerJump() {
-        if (this.is_onGround) {
+        if (!this.is_inwater && this.is_onGround) {
             this.is_onGround = false;
-            cc.find("gameMgr").getComponent("gameMgr").play_player_jump_sound();
-            this.getComponent(cc.RigidBody).linearVelocity = cc.v2(this.getComponent(cc.RigidBody).linearVelocity.x,this.jumpspeed);
+            cc.find("GameMgr").getComponent("gameMgr").play_player_jump_sound();
+            this.getComponent(cc.RigidBody).linearVelocity = cc.v2(this.getComponent(cc.RigidBody).linearVelocity.x, this.jumpspeed);
         }
     }
 
     playerDie() {
         Global.life--;
         this.is_die = true;
-        cc.audioEngine.playEffect(this.game_over_sound, false);
-
+        cc.find("gameMgr").getComponent("gameMgr").play_player_die_sound();
         if (Global.life < 0) {
             let seq = cc.sequence(
                 cc.callFunc(() => {
@@ -98,7 +113,7 @@ export default class Player extends cc.Component {
                 }),
                 cc.delayTime(0.5),
                 cc.callFunc(() => {
-                    cc.director.loadScene("GameOver");
+                    cc.director.loadScene("game_over");
                 })
             );
             this.node.runAction(seq);
@@ -116,7 +131,7 @@ export default class Player extends cc.Component {
             );
             this.node.runAction(seq);
         }
-    }   
+    }
 
     onBeginContact(contact, selfCollider, otherCollider) {
         if (this.is_die) {
@@ -124,12 +139,10 @@ export default class Player extends cc.Component {
         } else {
             if (otherCollider.node.name === "lower_bound") {
                 this.playerDie();
-                cc.audioEngine.playEffect(this.game_over_sound, false);
             } else if (otherCollider.tag === 2 && contact.getWorldManifold().normal.y < 0) {
                 this.is_onGround = true;
             } else if (otherCollider.node.name === "flag") {
-                console.log("game complete");
-                cc.find("gameMgr").getComponent("gameMgr").game_complete();
+                this.game_mgr.getComponent("gameMgr").game_complete();
             }
         }
     }
@@ -143,20 +156,36 @@ export default class Player extends cc.Component {
     onKeyDown(event: cc.Event.EventKeyboard) {
         switch (event.keyCode) {
             case cc.macro.KEY.a:
-                this.playerMove(-1);
+                this.playerMove(cc.v2(-1, this.moveDir.y));
                 break;
             case cc.macro.KEY.d:
-                this.playerMove(1);
+                this.playerMove(cc.v2(1, this.moveDir.y));
+                break;
+            case cc.macro.KEY.w:
+                if (this.is_inwater) {
+                    this.playerMove(cc.v2(this.moveDir.x, 1));
+                }
                 break;
             case cc.macro.KEY.space:
-                this.playerJump();
+                if(!this.is_inwater)
+                {
+                    this.playerJump();
+                }
                 break;
         }
     }
 
     onKeyUp(event: cc.Event.EventKeyboard) {
-        if (event.keyCode === cc.macro.KEY.a || event.keyCode === cc.macro.KEY.d) {
-            this.playerMove(0);
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.d:
+                this.playerMove(cc.v2(0, this.moveDir.y));
+                break;
+            case cc.macro.KEY.w:
+                if (this.is_inwater) {
+                    this.playerMove(cc.v2(this.moveDir.x, 0));
+                }
+                break;
         }
     }
 }
